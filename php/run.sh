34ded6637e7cd2cc60c652d7b8c2d37679d2154e
Pedro Cambra <pedro.cambra@gmail.com>
2016-08-12 10:48:56 +0900

    Initial commit

---
+++ php/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Dev environment operations.
+drush -y en stage_file_proxy devel
+drush vset stage_file_proxy_origin 'https://www.oist.jp'
+drush vset stage_file_proxy_use_imagecache_root 0
+
+# Run php fpm
+php-fpm7 --allow-to-run-as-root --nodaemonize --fpm-config /etc/php7/php-fpm.conf
