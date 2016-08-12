f61a65c75be6cb6c2fead02e3f41a298659089e7
Pedro Cambra <pedro.cambra@gmail.com>
2016-08-12 12:11:50 +0900

    Updated uid 1 email address

--- php/run.sh
+++ php/run.sh
@@ -4,6 +4,8 @@
 drush -y en stage_file_proxy devel
 drush vset stage_file_proxy_origin 'https://www.oist.jp'
 drush vset stage_file_proxy_use_imagecache_root 0
+# Replace by a generic one
+drush sqlq "UPDATE users SET mail = 'pedro.cambra@oist.jp' WHERE uid = 1"
 
 # Run php fpm
 php-fpm7 --allow-to-run-as-root --nodaemonize --fpm-config /etc/php7/php-fpm.conf
