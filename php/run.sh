49aef83de7eab9b8105433fa4d9f161e0d6cf618
Pedro Cambra <pedro.cambra@gmail.com>
2016-08-16 11:24:01 +0900

    Added a semaphore to the drush commands

--- php/run.sh
+++ php/run.sh
@@ -1,11 +1,29 @@
 #!/bin/bash
 
+# Set the right owner and permissions.
+chown -R www-data:www-data /var/www/html
+chmod -R go+w /var/www/html
+
+# Sort of semaphore process to ensure that the db is there when we run the drush
+# commands.
+BOOTSTRAP=`drush status | grep "Drupal bootstrap                :  Successful" | wc -l`
+until [ "$BOOTSTRAP" -ge "1" ]; do
+  >&2 echo "Waiting for the database to be ready"
+  BOOTSTRAP=`drush status | grep "Drupal bootstrap                :  Successful" | wc -l`
+  sleep 5
+done
+>&2 echo "Running drush commands"
+
 # Dev environment operations.
 drush -y en stage_file_proxy devel
 drush vset stage_file_proxy_origin 'https://www.oist.jp'
 drush vset stage_file_proxy_use_imagecache_root 0
 # Replace by a generic one
 drush sqlq "UPDATE users SET mail = 'pedro.cambra@oist.jp' WHERE uid = 1"
+echo "--------------------------------------------------------------------------"
+echo "****************************** Here's a uli ******************************"
+drush uli
+echo "--------------------------------------------------------------------------"
 
 # Run php fpm
 php-fpm7 --allow-to-run-as-root --nodaemonize --fpm-config /etc/php7/php-fpm.conf
